b5fee3ecd17f4351f6742020d2ae67f6497a69f0
Brooke Huntington <39963109+bthuntington@users.noreply.github.com>
2019-04-24 14:30:14 -0700

    meh

--- fw.py
+++ fw.py
@@ -38,8 +38,7 @@ def floydWarshall(graph):
   
 				# If vertex k is on the shortest path from  
 				# i to j, then update the value of dist[i][j] 
-				dist[i][j] = min(dist[i][j] , 
-								  dist[i][k]+ dist[k][j]) 
+				dist[i][j] = min(dist[i][j], dist[i][k]+ dist[k][j]) 
 	printSolution(dist) 
   
   
